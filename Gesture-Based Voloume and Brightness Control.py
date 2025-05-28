4073affc12b7ce3051e063ecec447981df922794
Anyasree <anyasreeaneesh2010@gmail.com>
2025-05-28 17:35:15 +0200

    .py
    .py

--- Gesture-Based Voloume and Brightness Control.py
+++ Gesture-Based Voloume and Brightness Control.py
@@ -19,4 +19,47 @@ try:
     max_vol = volume_range(1)
 
 expect Exception as e:
-    
+    print(f"Error initialising Pycaw: {e}")
+    exit()
+
+cap = cv2.VideoCapture(0)
+if not cap.isOpened():
+    print("Error coyuld not access the webcam")
+    exit()
+
+while True:
+    sucess, img = cap.read()
+    if not success:
+        print("Failed top read frame from webcam.")
+        break
+
+    img = cv2.flip(img, 1)
+    img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    results = hands.process(img_rgb)
+
+    if results.multi_hand_landmarks and results.multi_handedness:
+        for i, hand_landmarks in enumerate(results.multi_hand_landmarks):
+            hand_label = results. multi_handedness[i].classification[0].label
+        
+            mp_draw.draw_landmarks(img,hand_landmarks, mp_hands.HAND_CONNECTIONS)
+            thumb_tip = hand.landmarks.landmark[mp.hands.HandLandmark.THUMB_TIP]
+            index_tip = hand.landmarks.landmark[mp.hands.HandLandmark.INDEX_FINGER_TIP]
+
+            h, w, _ = img.shape
+            thumb_pos = (int(thumb_tip.x * w), int(thumb_tip.y * h))
+            index_pos = (int(index_tip.x * w), int(index_tip.y * h))
+            
+            cv2.circle(img, thumb_pos, 10, (255, 0, 0), cv2.FILLED)
+            cv2.circle(img, index_pos, 10, (255, 0, 0), cv2.FILLED)
+            cv2.line(img, thumb_pos, index_pos, (0, 255, 0), 3)
+
+            distance = hypot(index_pos[0] - thumb_pos(0), index_pos[1] - thumb_pos[1])
+            if hand_label == "Right":
+                vol = np.interp(distance, [30, 300], [min_vol], [max_vol])
+                try:
+                    volume.SetMasterVolumeLevel(vol, None)
+                except Eception as e:
+                        print("Error Adjusting Volume: {e}")
+
+                vol_bar = interp(distance, [30, 300], [400, 150])
+                cv2. rectangle(img, (50, 150), (85, 400), (255, 0, 0), 2)
--- gesture control.py
+++ gesture control.py
@@ -22,7 +22,7 @@ while True:
     
     mask = cv2.inRange(hsv, lower_skin, upper_skin)
     result = cv2.bitwise_and(frame, frame, mask=mask)
-    contours,_= cv2.flindContours (mask, cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
+    contours,_= cv2.findContours (mask, cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
 
     if contours:
         max_contour = max(contours,key = cv2.contourArea)
@@ -32,7 +32,7 @@ while True:
             cv2.rectangle(frame, (x, y),(x + w, y+h), (0,255, 0), 2)
 
             center_x = int(x + w/ 2)
-            center_x = int(y + h/ 2)
+            center_y = int(y + h/ 2)
             cv2.circle(frame, (center_x, center_y), 5, (0, 0, 255), -1)
 
     cv2.imshow('Original Frame', frame)
