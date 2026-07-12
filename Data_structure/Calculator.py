af3cf106cd52cf4ee0db268b1f9bbd7c28cabcea
Anyasree <anyasreeaneesh2010@gmail.com>
2026-07-12 07:29:19 -0700

    .py
    .py

---
+++ Data_structure/Calculator.py
@@ -0,0 +1,25 @@
+def add(a, b):
+    return a + b
+def subtract(a, b):
+    return a - b
+def multiply(a, b):
+    return a * b
+def divide(a, b):
+    return a / b
+
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+choice = input("Choose operation (+, -, *, /)")
+
+if choice == "+":
+    print(add(num1, num2))
+elif choice == "-":
+    print(subtract(num1, num2))
+elif choice == "*":
+    print(multiply(num1, num2))
+elif choice == "/":
+    print(divide(num1, num2))
+else:
+    print("Invalid operation")
+    
---
+++ Data_structure/Introduce_Yourself.py
@@ -0,0 +1,7 @@
+def introduce(name):
+    print("Hello, my name is", name)
+    print("Nice to meet you!")
+
+user = input("Enter your name: ")
+
+introduce(user)
